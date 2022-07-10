2482fbbae9e8fc8a392689359174449c4aaf62cf
ConcreditoConsignados <privadozthedragonz@gmail.com>
2022-07-10 18:11:36 -0300

    update

--- src/Utils/functions.js
+++ src/Utils/functions.js
@@ -226,6 +226,8 @@ removeCaracteresSpeciais = function(s){
   r = r.replace(new RegExp("[òóôõö]", 'g'),"o");
   r = r.replace(new RegExp("[ùúûü]", 'g'),"u");
   r = r.replace(new RegExp("[ýÿ]", 'g'),"y");
+  r = r.replace(new RegExp("[']", 'g'),"");
+  r = r.replace(new RegExp('["]', 'g'),"");
   return r;
 };
 
