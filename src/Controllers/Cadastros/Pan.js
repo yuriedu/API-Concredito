a2802229de098f306e9aa0147ba81e81322d1aa7
Yuri Eduardo <privadozthedragonz@gmail.com>
2022-07-18 15:30:04 -0300

    update

--- src/Controllers/Cadastros/Pan.js
+++ src/Controllers/Cadastros/Pan.js
@@ -26,10 +26,10 @@ const PanFGTS = async (cliente, pool, log) => {
               table = '900006'
             } else if (client.Tabela.includes('900007')) {
               table = '900007'
-            } else return execSQL(pool, cliente, cliente.IdContrato, 824, '', `[7]=> Não cadastro propostas PANAMERICANO nessa tabela!`)
+            } else return saveDB(pool, cliente.IdContrato, 824, '', '[7]=> Não cadastro propostas PANAMERICANO nessa tabela!', false)
             const tabela = simularProposta.data[0].condicoes_credito.find(element => element.codigo_tabela_financiamento == table)
             if (parseFloat(tabela.valor_cliente) - client.Valor > client.Valor*0.05) return saveDB(pool, cliente.IdContrato, 824, '', `[6]=> Valor simulado é mais de 5% menor que o proposto ao cliente! Altere o valor e tente novamente... Valor da simulação: ${calcularSaldo.data.valor_liquido}`, false)
-            if (bancoTranslate(client.CodBancoCliente) == 104) return execSQL(pool, cliente, cliente.IdContrato, 824, '', `[7]=> A API da Pan não aceita Caixa Economica Federal! Faça MANUALMENTE...`)
+            if (bancoTranslate(client.CodBancoCliente) == 104) return saveDB(pool, cliente.IdContrato, 824, '', '[7]=> A API da Pan não aceita Caixa Economica Federal! Faça MANUALMENTE...', false)
             const clienteDados = {
               cpf_cliente: data.cpf_cliente,
               telefones: [{
