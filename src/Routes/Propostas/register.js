7dff4d676be4417c30b9a242932395e8455ccf55
Yuri Eduardo <privadozthedragonz@gmail.com>
2022-07-19 15:12:17 -0300

    update

--- src/Routes/Propostas/register.js
+++ src/Routes/Propostas/register.js
@@ -66,6 +66,7 @@ const registerPropostas = async (req, res, logs) => {
         element.NomePai = await fixName(element.NomePai)
         element.NomePai = await removeCaracteresSpeciais(element.NomePai)
       } else element.NomePai = 'nao identificado'
+      if (element.Cidade) element.Cidade = element.Cidade.replaceAll("'","").replaceAll('"','')
       if (!element.EndNumero || element.EndNumero == 'SN' || element.EndNumero == 'S/N' || element.EndNumero == 0 || element.EndNumero == '0' || element.EndNumero == '00') element.EndNumero = '01'
 
       var response = false;
