9fbdc0dea8d1225d34ff4ec0f78340adc91adc69
Yuri Eduardo <privadozthedragonz@gmail.com>
2022-07-21 09:36:34 -0300

    update

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ async function Esteira() {
     FactaEsteira(pool, {af: "FACTA ESTEIRA"})
     //C6Esteira(pool, {af: "C6 ESTEIRA"})
   },600000)
+  FactaEsteira(pool, {af: "FACTA ESTEIRA"})
   //const { PanEsteira } = require('./src/Controllers/Esteiras/Pan')
   //await PanEsteira(pool, {af: "PAN ESTEIRA"})
 }
